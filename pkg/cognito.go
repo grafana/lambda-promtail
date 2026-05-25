ee242b1350ecb7fa11d14280e5bcf11d3cb0e2bb
Rogelio <rogelio.ramos@indivi.io>
2026-05-25 13:50:53 +0200

    Wrap AWS Cognito Logs errors

--- pkg/cognito.go
+++ pkg/cognito.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/grafana/loki/v3/pkg/logproto"
@@ -15,12 +16,12 @@ func parseCognitoRecord(record Record) (logproto.Entry, error) {
 	}
 	document, err := json.Marshal(record.Content)
 	if err != nil {
-		return logproto.Entry{}, err
+		return logproto.Entry{}, fmt.Errorf("failed to marshal cognito record content: %w", err)
 	}
 	if val, ok := record.Content["eventTimestamp"]; ok {
 		sec, nsec, err := getUnixSecNsec(val.(string))
 		if err != nil {
-			return logproto.Entry{}, err
+			return logproto.Entry{}, fmt.Errorf("failed to parse cognito eventTimestamp %q: %w", val, err)
 		}
 
 		timestamp = time.Unix(sec, nsec).UTC()
